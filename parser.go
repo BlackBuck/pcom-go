c60c8b1bd1fa47f8911d40e01c76e687bf6f3e44
Anil Bishnoi <anilkumarpanwar2929@gmail.com>
2025-01-11 02:02:59 +0530

    Making result and state public

--- parser.go
+++ parser.go
@@ -4,29 +4,39 @@ import (
 	"fmt"
 )
 
-// state defines the state of the current parsing logic.
+// State defines the State of the current parsing logic.
 // input is the input string.
 // offset is used to determine the position at which the next parser will start parsing.
-type state struct {
+type State struct {
 	input  string
 	offset int
 }
 
-// result struct will be returned by all parsers alongside an error (if present).
-// parsedResult is the parsed result of the parser.
+// Result struct will be returned by all parsers alongside an error (if present).
+// parsedResult is the parsed Result of the parser.
 // parsedResult should've (ideally) been a generic type, but that would create unnecessary overhead.
-// nextState is the state after the current parser is done parsing the input.
-type result struct {
+// nextState is the State after the current parser is done parsing the input.
+type Result struct {
 	parsedResult interface{}
-	nextState    state
+	nextState    State
+}
+
+// constructor for Result
+func NewResult(parsedResult interface{}, nextState State) Result {
+	return Result{parsedResult, nextState}
+}
+
+// constructor for State
+func NewState(input string, offset int) State {
+	return State{input, offset}
 }
 
 // The Parser type should've(ideally) been a struct, but with generic types it created a LOT of overhead.
-type Parser func(curState state) (result, error)
+type Parser func(curState State) (Result, error)
 
 // advance n places
-func (s state) advance(n int) state {
-	return state{
+func (s State) advance(n int) State {
+	return State{
 		s.input,
 		s.offset + n,
 	}
@@ -34,43 +44,43 @@ func (s state) advance(n int) state {
 
 // basic char parser
 func CharParser(c byte) Parser {
-	return func(curState state) (result, error) {
+	return func(curState State) (Result, error) {
 		if curState.offset >= len(curState.input) {
-			return result{
+			return NewResult(
 				nil,
 				curState,
-			}, fmt.Errorf("reached the end of input string while parsing")
+			), fmt.Errorf("reached the end of input string while parsing")
 		}
 
 		if curState.input[curState.offset] != c {
-			return result{
+			return NewResult(
 				nil,
 				curState,
-			}, fmt.Errorf("expected %c but received %c", c, curState.input[curState.offset])
+			), fmt.Errorf("expected %c but received %c", c, curState.input[curState.offset])
 		}
 
-		return result{
+		return NewResult(
 			string(c),
 			curState.advance(1),
-		}, nil
+		), nil
 	}
 }
 
 // @params s (string).
-// It parses a string exactly and advances the current state.
+// It parses a string exactly and advances the current State.
 func String(s string) Parser {
-	return func(curState state) (result, error) {
+	return func(curState State) (Result, error) {
 		if curState.input[curState.offset:] != s {
-			return result{
+			return NewResult(
 				nil,
 				curState,
-			}, fmt.Errorf("expected %s", s)
+			), fmt.Errorf("expected %s", s)
 		}
 
-		return result{
+		return NewResult(
 			s,
 			curState.advance(len(s)),
-		}, nil
+		), nil
 	}
 }
 
@@ -79,7 +89,7 @@ func String(s string) Parser {
 // @returns Parser.
 // performs a logical OR operation between the left and right parsers.
 func Or(left Parser, right Parser) Parser {
-	return func(curState state) (result, error) {
+	return func(curState State) (Result, error) {
 		leftRes, err := left(curState)
 		if err != nil {
 			curState = leftRes.nextState
@@ -94,31 +104,31 @@ func Or(left Parser, right Parser) Parser {
 // @returns Parser.
 // performs a logical AND operation between the left and right parsers.
 func And(left Parser, right Parser) Parser {
-	return func(curState state) (result, error) {
+	return func(curState State) (Result, error) {
 		leftRes, err := left(curState)
 		if err != nil {
-			return result{
+			return NewResult(
 				nil,
 				curState,
-			}, fmt.Errorf("err in parsing \"and\"")
+			), fmt.Errorf("err in parsing \"and\"")
 		}
 
 		// Don't assign leftRes.nextState directly to curState
-		// because if right parser results in an error, we
+		// because if right parser Results in an error, we
 		// won't have anything for fallback
 		next := leftRes.nextState
 		rightRes, err := right(next)
 		if err != nil {
-			return result{
+			return NewResult(
 				nil,
 				curState,
-			}, fmt.Errorf("err in parsing \"and\"")
+			), fmt.Errorf("err in parsing \"and\"")
 		}
 
-		return result{
+		return NewResult(
 			[]interface{}{leftRes.parsedResult, rightRes.parsedResult},
 			rightRes.nextState,
-		}, nil
+		), nil
 	}
 }
 
@@ -127,20 +137,20 @@ func And(left Parser, right Parser) Parser {
 // @returns Parser.
 // It maps the output of the parser(p) through the mappping func.
 func Map[A, B any](p Parser, mapping func(A) B) Parser {
-	return func(curState state) (result, error) {
+	return func(curState State) (Result, error) {
 		res, err := p(curState)
 
 		if err != nil {
-			return result{
+			return NewResult(
 				nil,
 				curState,
-			}, err
+			), err
 		}
 
-		return result{
+		return NewResult(
 			mapping(res.parsedResult.(A)),
 			res.nextState,
-		}, nil
+		), nil
 	}
 }
 
@@ -148,24 +158,24 @@ func Map[A, B any](p Parser, mapping func(A) B) Parser {
 // @returns Parser
 // It checks for the presence of zero or more occurences of the parser in the input
 func Many0(p Parser) Parser {
-	return func(curState state) (result, error) {
+	return func(curState State) (Result, error) {
 		var res []interface{}
 		for curState.offset < len(curState.input) {
 			x, err := p(curState)
 			if err != nil {
-				return result{
+				return NewResult(
 					res,
 					curState, 
-				}, nil
+				), nil
 			}
 			res = append(res, x.parsedResult)
 			curState = x.nextState
 		}
 
-		return result{
+		return NewResult(
 			res,
 			curState,
-		}, nil
+		), nil
 	}
 }
 
@@ -173,31 +183,31 @@ func Many0(p Parser) Parser {
 // @returns Parser.
 // It checks for the presence of one or more occurence of the parser in the input.
 func Many1(p Parser) Parser {
-	return func(curState state) (result, error) {
+	return func(curState State) (Result, error) {
 		var res []interface{}
 		for curState.offset < len(curState.input) {
 			x, err := p(curState)
 			if err != nil {
 				if len(res) == 0 {
-					return result{
+					return NewResult(
 						nil,
 						curState,
-					}, err
+					), err
 				}
 
-				return result{
+				return NewResult(
 					res,
 					x.nextState,
-				}, nil
+				), nil
 			}
 			res = append(res, x.parsedResult)
 			curState = x.nextState
 		}
 
-		return result{
+		return NewResult(
 			res,
 			curState,
-		}, nil
+		), nil
 	}
 }
 
@@ -206,25 +216,25 @@ func Many1(p Parser) Parser {
 // It sequentially parses the input.
 // The output of the first parser goes as input for the second and so on.
 func Seq(parsers ...Parser) Parser {
-	return func(curState state) (result, error) {
+	return func(curState State) (Result, error) {
 		var res []interface{}
 		next := curState
 		for _, parser := range parsers {
 			x, err := parser(next)
 			if err != nil {
-				return result{
+				return NewResult(
 					nil,
-					curState, // fallback to the initial state
-				}, err
+					curState, // fallback to the initial State
+				), err
 			}
 			res = append(res, x.parsedResult)
 			next = x.nextState
 		}
 
-		return result{
+		return NewResult(
 			res,
 			next,
-		}, nil
+		), nil
 	}
 }
 
@@ -232,19 +242,19 @@ func Seq(parsers ...Parser) Parser {
 // @returns Parser.
 // It checks for the presence of zero or one occurence of the parser in the input.
 func Optional(p Parser) Parser {
-	return func(curState state) (result, error) {
+	return func(curState State) (Result, error) {
 		res, err := p(curState)
 		if err != nil {
-			return result{
+			return NewResult(
 				nil,
 				curState,
-			}, nil
+			), nil
 		}
 
-		return result{
+		return NewResult(
 			res.parsedResult,
 			res.nextState,
-		}, nil
+		), nil
 	}
 }
 
@@ -253,35 +263,35 @@ func Optional(p Parser) Parser {
 // It parses only the input that is present between open and close.
 // It then returns the output produced by the context parser.
 func Between(open, content, close Parser) Parser {
-	return func(curState state) (result, error) {
+	return func(curState State) (Result, error) {
 		openRes, err := open(curState)
 		if err != nil {
-			return result{
+			return NewResult(
 				nil,
 				curState,
-			}, err
+			), err
 		}
 
 		contentRes, err := content(openRes.nextState)
 		if err != nil {
-			return result{
+			return NewResult(
 				nil,
 				curState,
-			}, err
+			), err
 		}
 
 		closeRes, err := close(contentRes.nextState)
 		if err != nil {
-			return result{
+			return NewResult(
 				nil, 
 				curState,
-			}, err
+			), err
 		}
 
-		return result{
+		return NewResult(
 			contentRes.parsedResult,
 			closeRes.nextState,
-		}, nil
+		), nil
 	}
 }
 
@@ -290,7 +300,7 @@ func Between(open, content, close Parser) Parser {
 // It delays the creation of a parser unless required.
 func Lazy(f func() Parser) Parser {
 	var memo Parser
-	return func(curState state) (result, error) {
+	return func(curState State) (Result, error) {
 		if memo == nil {
 			memo = f()
 		} 
